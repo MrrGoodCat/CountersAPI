0f2499fcd329fac45c114e9b26b61779100daee7
MrrGoodCat <mrrgoodcat@gmail.com>
2018-06-26 00:59:35 +0300

    CPU

---
+++ CountersAPI/Controllers/CPUCounterController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CountersAPI.Controllers
+{
+    public class CPUCounterController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            var result = 0;
+
+            var performanceCounterCategories = PerformanceCounterCategory.GetCategories().FirstOrDefault(category => category.CategoryName == "Processor Information");
+            //var performanceCounters = performanceCounterCategories.GetCounters("_Total");
+
+            //result = performanceCounters.GetValue().ToString();
+
+            if (performanceCounterCategories.CounterExists("% Processor Time"))
+            {
+                var performanceCounter = new PerformanceCounter(performanceCounterCategories.CategoryName, "% Processor Time", "_Total");
+                using (performanceCounter)
+                {
+                    result = (int)performanceCounter.NextValue();
+                }
+            }
+
+            return Ok(result);
+        }
+    }
+}
